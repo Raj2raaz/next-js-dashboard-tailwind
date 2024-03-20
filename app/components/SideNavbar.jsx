e711bd1b7147f8245a8ea04eb94f817f5b187baf
Subham <rajshubham199@gmail.com>
2024-03-21 02:48:32 +0530

    some changes in sidenavbar

--- app/components/SideNavbar.jsx
+++ app/components/SideNavbar.jsx
@@ -1,30 +1,75 @@
-import React from 'react'
-import PersonIcon from '@mui/icons-material/Person';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import ChatIcon from '@mui/icons-material/Chat';
-import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+import React from "react";
+import PersonIcon from "@mui/icons-material/Person";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ChatIcon from "@mui/icons-material/Chat";
+import LocalGroceryStoreIcon from "@mui/icons-material/LocalGroceryStore";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import SettingsSuggestIcon from "@mui/icons-material/SettingsSuggest";
+import ImageSearchIcon from "@mui/icons-material/ImageSearch";
+import EventIcon from "@mui/icons-material/Event";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+
+import CompressIcon from '@mui/icons-material/Compress';
 
 const SideNavbar = () => {
   return (
     // <div className='flex-none w-200 bg-gray-50 h-screen text-black'>
-    <div className='flex-none bg-gray-50 h-screen text-black' style={{ width: '220px' }}>
-      <div className='flex justify-between'>
-        <div><PersonIcon/><span className='pl-3'>Hello User</span></div>
-        <div><NotificationsIcon/></div>
-      </div>
-      
-      <ul>
-        <li className='flex'> <ChatIcon/> <p className='pl-3'>Disscussion Fourm</p></li>
-        <li className='flex'> <LocalGroceryStoreIcon/> <p className='pl-3'>Market Stories</p></li>
-        <li className='pl-10'>Sentiment</li>
-        <li className='pl-10'>Market</li>
-        <li className='pl-10'>Sector</li>
-        <li className='pl-10'>Watchlist</li>
-        <li className='pl-10'>Events</li>
-        <li className='pl-10'>News/Interview</li>
-      </ul>
-    </div>
-  )
-}
+    <>
+      <div
+        className="flex-none bg-gray-50 h-screen text-black"
+        style={{ width: "250px", alignItems: "center" }}
+      >
+        <div className="flex justify-between px-6 py-8 items-center border-b">
+          <div>
+            <PersonIcon />
+            <span className="pl-3">Hello User</span>
+          </div>
+          <div>
+            <NotificationsIcon />
+          </div>
+        </div>
 
-export default SideNavbar
+        <ul className="px-6 ">
+          <li className="flex">
+            {" "}
+            <ChatIcon /> <p className="pl-3">Disscussion Fourm</p>
+          </li>
+          <li className="flex">
+            {" "}
+            <LocalGroceryStoreIcon /> <p className="pl-3">Market Stories</p>
+          </li>
+          <li className="flex">
+            <SentimentSatisfiedAltIcon />
+            <p className="pl-3">Sentiment</p>
+          </li>
+          <li className="flex">
+            <StorefrontIcon />
+            <p className="pl-3">Market</p>
+          </li>
+          <li className="flex">
+            <SettingsSuggestIcon />
+            <p className="pl-3">Sector</p>
+          </li>
+          <li className="flex">
+            <ImageSearchIcon />
+            <p className="pl-3">Watchlist</p>
+          </li>
+          <li className="flex">
+            <EventIcon />
+            <p className="pl-3">Events</p>
+          </li>
+          <li className="flex">
+            <NewspaperIcon />
+            <p className="pl-3">News/Interview</p>
+          </li>
+        </ul>
+      </div>
+      <div className='flex w-200 bg-gray-50 h-screen text-black items-center justify-center'>
+        <CompressIcon style={{ transform: 'rotate(90deg)' }} />
+      </div>
+    </>
+  );
+};
+
+export default SideNavbar;
